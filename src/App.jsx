ca66c106799bccbb33c5cbbab35f9ab5364c3680
Aryan <114978467+minkxx@users.noreply.github.com>
2025-06-13 22:58:55 +0530

    Update loading GIF source path in App component

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
   if (!currentWeatherData) {
     return (
       <div className="min-h-screen bg-slate-900 flex items-center justify-center">
-        <img src="src\assets\loading.gif" alt="loading..." />
+        <img src="/loading.gif" alt="loading..." />
       </div>
     );
   }
